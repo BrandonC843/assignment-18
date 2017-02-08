51134e522772de41b532e32ff491aa42eac70634
Brandon Callahan <brandoncallahan@Brandons-MacBook-Pro.local>
2017-02-08 14:42:26 -0500

    initial files

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-console.log('wat')
+import $ from 'jquery';
+
+
+
+$.getJSON('http://apis.is/concerts').then(function(serverRes){
+
+
+
+})
+
+$.getJSON('http://apis.is/rides/samferda-drivers/').then(function(serverRes){
+console.log(serverRes)
+
+})
+
+$.getJSON('http://apis.is/flight?language=en&type=departures').then(function(serverRes){
+
+
+
+})
